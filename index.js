94c091954b94e010554920fec19baabc775f72e0
S.P. ROY <sprithi44@gmail.com>
2023-03-21 23:20:10 -0400

    Hello World deploy

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Tech Tonic server is listening on port ${port}.`);
+});
