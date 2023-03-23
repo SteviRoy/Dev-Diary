8903a2175732c39bff301f4c619e575b915b8ff9
S.P. ROY <sprithi44@gmail.com>
2023-03-22 20:30:41 -0400

    delete a post added

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -66,3 +66,25 @@ router.put('/:id', withAuth, async (req, res) => {
   }
 });
 
+// Delete a post
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const deletedPost = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!deletedPost) {
+      res.status(404).json({ message: 'No post found with that id!' });
+      return;
+    }
+
+    res.status(200).json(deletedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
