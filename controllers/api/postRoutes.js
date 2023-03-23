cc2b39cf9d2096dcb36a45ff043a3202bbdae5ca
S.P. ROY <sprithi44@gmail.com>
2023-03-22 20:20:06 -0400

    create a new post added

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -32,3 +32,16 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// Create a new post
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
