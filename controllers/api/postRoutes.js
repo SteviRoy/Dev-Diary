c1a8cd72fd8f43e4a4b59cb5df34519815876bca
S.P. ROY <sprithi44@gmail.com>
2023-03-22 20:12:42 -0400

    get a post by id added

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -14,3 +14,21 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Get a single post
+router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: User, attributes: ['username'] }],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with that id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
